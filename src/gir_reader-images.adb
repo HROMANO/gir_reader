86545406a8db26ee88f7329ba18e3ba3acd12e60
Hervé ROMANO <shuihuzhuan@free.fr>
2025-12-19 11:17:36 +0100

    Add To_String functions + indent text output
    Signed-off-by: Hervé ROMANO <shuihuzhuan@free.fr>

--- src/gir_reader-images.adb
+++ src/gir_reader-images.adb
@@ -2,6 +2,13 @@ with Ada.Strings.Fixed;
 
 package body Gir_Reader.Images is
 
+   ---------------
+   --  To_String  --
+   ---------------
+
+   function To_String (Item : Boolean) return Utf8
+   is (if Item then -"true" else -"false");
+
    -------------
    --  Image  --
    -------------
@@ -10,12 +17,15 @@ package body Gir_Reader.Images is
      (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
       Item   : Boolean) is
    begin
-      if Item then
-         Output.Put (-"true");
-      else
-         Output.Put (-"false");
-      end if;
+      Output.Put (To_String (Item));
    end Image;
+
+   ---------------
+   --  To_String  --
+   ---------------
+
+   function To_String (Item : Integer) return Utf8
+   is (Ada.Strings.Fixed.Trim (Item'Image, Ada.Strings.Both));
 
    -------------
    --  Image  --
@@ -25,8 +35,45 @@ package body Gir_Reader.Images is
      (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
       Item   : Integer) is
    begin
-      Output.Put (Ada.Strings.Fixed.Trim (Item'Image, Ada.Strings.Both));
+      Output.Put (To_String (Item));
    end Image;
+
+   ---------------
+   --  To_String  --
+   ---------------
+
+   function To_String
+     (Item : Text; Indent : Utf8 := ""; Indent_First_Line : Boolean := False)
+      return Utf8
+   is
+      Result  : Text := Item;
+      Start   : Positive := 1;
+      Index   : Natural := 0;
+      Pattern : String := "" & ASCII.LF;
+   begin
+
+      if Ada.Strings.Unbounded.Length (Item) = 0 then
+         return "";
+      end if;
+
+      if Indent_First_Line then
+         Ada.Strings.Unbounded.Insert (Result, 1, Indent);
+      end if;
+
+      while True loop
+
+         Index := Ada.Strings.Unbounded.Index (Result, Pattern, Start);
+
+         exit when Index = 0;
+
+         Start := Index + 1;
+         Ada.Strings.Unbounded.Insert (Result, Start, Indent);
+
+      end loop;
+
+      return """" & (+Result) & """";
+
+   end To_String;
 
    -------------
    --  Image  --
@@ -34,9 +81,17 @@ package body Gir_Reader.Images is
 
    procedure Image
      (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
-      Item   : Text) is
+      Item   : Text)
+   is
+
+      use Ada.Strings.Fixed;
+
+      Indent : constant Utf8 :=
+        (Natural (Output.Current_Indent)
+         + Gir_Reader.Images.Default_Indent_Size)
+        * " ";
    begin
-      Output.Put ("""" & (+Item) & """");
+      Output.Put (To_String (Item, Indent));
    end Image;
 
 end Gir_Reader.Images;
--- src/gir_reader-images.ads
+++ src/gir_reader-images.ads
@@ -1,5 +1,5 @@
---  This package provides custom Image procedures for Boolean, Integer and
---  Text types.
+--  This package provides custom To_String and Image procedures for Boolean,
+--  Integer and Text types.
 
 pragma Ada_2022;
 
@@ -7,13 +7,23 @@ with Ada.Strings.Text_Buffers;
 
 package Gir_Reader.Images is
 
+   Default_Indent_Size : constant Natural := 3;
+
+   function To_String (Item : Boolean) return Utf8;
+
    procedure Image
      (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
       Item   : Boolean);
 
+   function To_String (Item : Integer) return Utf8;
+
    procedure Image
      (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
       Item   : Integer);
+
+   function To_String
+     (Item : Text; Indent : Utf8 := ""; Indent_First_Line : Boolean := False)
+      return Utf8;
 
    procedure Image
      (Output : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class;
